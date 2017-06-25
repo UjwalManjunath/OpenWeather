e91a3ed44a3089907d78a50c2e0ab9f0ff6d06db
ujwalmanjunath <muddu1990@gmail.com>
2017-06-25 11:36:35 +0530

    Project init

---
+++ OpenWeather/OpenWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenWeather
+//
+//  Created by Ujwal Manjunath on 6/25/17.
+//  Copyright © 2017 Win-kel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
